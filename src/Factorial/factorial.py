fa78afca4eeaff74c9da8732ffbdb3ab516891f3
TiagoOdiard <tiagoodiard345@gmail.com>
2026-03-27 16:20:25 -0300

    Agregando funcion de rangos en factorial.py

--- src/Factorial/factorial.py
+++ src/Factorial/factorial.py
@@ -5,14 +5,12 @@
 #* Dr.P.E.Colla (c) 2022                                                   *
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
+
 import sys
+
 def factorial(num): 
-    if num < 0: 
-        print("Factorial de un número negativo no existe")
-        return 0
-    elif num == 0: 
-        return 1
-        
+    if num < 0: return 0
+    elif num == 0: return 1
     else: 
         fact = 1
         while(num > 1): 
@@ -20,10 +18,31 @@ def factorial(num):
             num -= 1
         return fact 
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
+def procesar_rango(rango_str):
+    try:
+        # Separamos los números usando el guion '-' como divisor
+        partes = rango_str.split('-')
+        desde = int(partes[0])
+        hasta = int(partes[1])
+        
+        # Validamos que el rango sea lógico 
+        if desde > hasta:
+            desde, hasta = hasta, desde 
+            
+        print(f"\nCalculando factoriales desde {desde} hasta {hasta}:")
+        for i in range(desde, hasta + 1):
+            print(f"  {i}! = {factorial(i)}")
+            
+    except (ValueError, IndexError):
+        print("Error: El formato debe ser NÚMERO-NÚMERO (ejemplo: 4-8)")
 
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
 
+# .Intento por argumento de terminal
+if len(sys.argv) > 1:
+    rango_input = sys.argv[1]
+else:
+    # .Si se omite, se solicita manualmente
+    print("No se detectó un rango como argumento.")
+    rango_input = input("Ingrese el rango (ej. 4-8): ")
+
+procesar_rango(rango_input)
